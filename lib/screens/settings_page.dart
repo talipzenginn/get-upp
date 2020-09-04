d3ed3cca550135d3aadbfef1d892638f1de497a6
Talip Zengin <talipzenginn@gmail.com>
2020-09-04 21:47:40 +0300

    Resolves settings screen design

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -1,14 +1,70 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart' show FlutterSwitch;
 import '../components/constants.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Settings',
-        style: optionStyle,
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: EdgeInsets.only(
+            top: 25.0,
+            right: 15.0,
+            left: 27.0,
+            bottom: 13.4,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'My Tasks Page Settings',
+                style: TextStyle(
+                  color: kTitleColor,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                    child: Text(
+                      'Hide completed tasks',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                  FlutterSwitch(
+                    width: 100.0,
+                    height: 40.0,
+                    valueFontSize: 16.0,
+                    toggleSize: 45.0,
+                    value: true,
+                    borderRadius: 30.0,
+                    padding: 8.0,
+                    showOnOff: true,
+                    inactiveColor: Colors.white24,
+                    activeColor: Colors.lightBlue,
+                    onToggle: (val) {},
+                  ),
+                ],
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
