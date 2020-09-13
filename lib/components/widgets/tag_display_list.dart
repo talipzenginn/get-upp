4b9945b44573c4756b247e660454969696afd7ff
Talip Zengin <talipzenginn@gmail.com>
2020-09-13 17:56:00 +0300

    Resolves adding delete function

--- lib/components/widgets/tag_display_list.dart
+++ lib/components/widgets/tag_display_list.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart'
     show FontAwesomeIcons;
-import 'package:provider/provider.dart' show Provider;
+import 'package:provider/provider.dart' show Consumer, Provider;
 import '../../providers/tags_list_provider.dart';
 import '../../screens/add_tag_screen.dart';
 
 class TagDisplayList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: Provider.of<TagsListProvider>(context).tagsCount,
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          leading: Icon(
-            FontAwesomeIcons.tag,
-            color: AddTagScreen().colors[
-                Provider.of<TagsListProvider>(context).tags[index].colorIndex],
-          ),
-          title: Text(Provider.of<TagsListProvider>(context).tags[index].name),
-        );
-      },
-    );
+    return Consumer<TagsListProvider>(builder: (context, tagData, child) {
+      return ListView.builder(
+        itemCount: tagData.tagsCount,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            onLongPress: () {
+              Provider.of<TagsListProvider>(context, listen: false)
+                  .deleteTag(tagData.tags[index]);
+            },
+            leading: Icon(
+              FontAwesomeIcons.tag,
+              color: AddTagScreen().colors[tagData.tags[index].colorIndex],
+            ),
+            title: Text(tagData.tags[index].name),
+          );
+        },
+      );
+    });
   }
 }
